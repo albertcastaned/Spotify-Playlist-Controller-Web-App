559515a176fd706ed2a27541a9e55159ed8c48a0
Alberto Castañeda <albertocastaneda@Apollo.lan>
2018-07-22 18:20:44 -0700

    Test

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -76,6 +76,7 @@ def vote():
 @main.route("/get-vote",methods=['POST'])
 def get_vote():
     try:
+        #lmaooo
         global index_add_counter # means: in this scope, use the global name
         pusher_client.trigger('chatchannel', 'update-count', {'count':index_add_counter})
         return jsonify({'result' : 'success'})
